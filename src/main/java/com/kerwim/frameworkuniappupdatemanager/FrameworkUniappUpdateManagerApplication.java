78f6c92bbadd302cb2e5de7db4df8e0371a06cfa
mac <0924>
2024-10-12 00:25:55 +0800

    intro

---
+++ src/main/java/com/kerwim/frameworkuniappupdatemanager/FrameworkUniappUpdateManagerApplication.java
@@ -0,0 +1,13 @@
+package com.kerwim.frameworkuniappupdatemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkUniappUpdateManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkUniappUpdateManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kerwim/frameworkuniappupdatemanager/FrameworkUniappUpdateManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerwim.frameworkuniappupdatemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkUniappUpdateManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
